035527f8565a6a64ead58db37ded80751b92c3ea
AlexisHolyoak <peraltaholyoak.aj@gmail.com>
2021-12-22 18:15:25 -0500

    fixed url

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get("/", function(req, res){
 app.get("/carnet/vacunacion/ef43fad1344f51/holyoak/yolanda", function (req, res) {
   res.sendFile(__dirname +"/public/"+"mama.html");
 });
-app.get("/carnet/vacunacion/peru/ef43fad1344f51/peralta/jesus", function (req, res) {
+app.get("/carnet/vacunacion/ef43fad1344f51/peralta/jesus", function (req, res) {
   res.sendFile(__dirname +"/public/"+"papa.html");
 });
 app.get("/carnet/vacunacion/ef43fad1344f51/peralta/alexis", function (req, res) {
