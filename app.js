12aab3ad4b0f9896f3c010762fb64e1b4ed9d658
AlexisHolyoak <peraltaholyoak.aj@gmail.com>
2021-12-22 18:06:12 -0500

    added sons

--- app.js
+++ app.js
@@ -8,13 +8,18 @@ app.use(express.static("public"))
 app.get("/", function(req, res){
   res.send("Hello");
 })
-app.get("/mama", function (req, res) {
+app.get("/carnet/vacunacion/ef43fad1344f51/holyoak/yolanda", function (req, res) {
   res.sendFile(__dirname +"/public/"+"mama.html");
 });
-app.get("/papa", function (req, res) {
+app.get("/carnet/vacunacion/peru/ef43fad1344f51/peralta/jesus", function (req, res) {
   res.sendFile(__dirname +"/public/"+"papa.html");
-})
-
+});
+app.get("/carnet/vacunacion/ef43fad1344f51/peralta/alexis", function (req, res) {
+  res.sendFile(__dirname +"/public/"+"alexis.html");
+});
+app.get("/carnet/vacunacion/ef43fad1344f51/peralta/diana", function (req, res) {
+  res.sendFile(__dirname +"/public/"+"diana.html");
+});
 // start the server listening for requests
 app.listen(process.env.PORT || 3000, 
 	() => console.log("Server is running..."));
